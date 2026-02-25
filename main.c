f80100bcac60086ef0f7dacee2b5552e2311dc96
Jaizzer <florenzjaizzer.calderon@gmail.com>
2026-02-25 14:09:53 +0800

    feat: Enable interrupt for timer 2

--- main.c
+++ main.c
@@ -1,3 +1,14 @@
-#include <stdint.h>
+#include "stm32f411xe.h"
 
-int main(void) { return 0; }
+int main(void) {
+    RCC->AHB1ENR |= RCC_APB1ENR_TIM2EN;
+    TIM2->PSC |= 15;
+    TIM2->ARR |= 999;
+    TIM2->DIER |= TIM_DIER_UIE;
+    TIM2->CR1 |= TIM_CR1_CEN;
+    NVIC_EnableIRQ(TIM2_IRQn);
+    NVIC_SetPriority(TIM2_IRQn, 1);
+
+    while (1) {
+    };
+}
